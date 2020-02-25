130fa9da3e41b80a14c8cb702ad4c7440d0804a7
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-25 22:44:20 +0200

    Dynamically filter @FetchRequest

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -15,36 +15,64 @@ struct Student: Hashable {
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")/*NSPredicate(format: "universe IN %@",["Aliens", "Firefly", "Star Trek"])*/) var ships: FetchedResults<Ship>
+    @State private var lastNameFilter = "A"
+//    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")/*NSPredicate(format: "universe IN %@",["Aliens", "Firefly", "Star Trek"])*/) var ships: FetchedResults<Ship>
 //    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
 //    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
     
     var body: some View {
         VStack {
-            List(ships, id: \.self) { ship in
-                Text(ship.name ?? "Unknown name")
-            }
+            FilteredList(filter: lastNameFilter)
             
             Button("Add Examples") {
-                let ship1 = Ship(context: self.moc)
-                ship1.name = "Enterprise"
-                ship1.universe = "Star Trek"
+                let taylor = Singer(context: self.moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
                 
-                let ship2 = Ship(context: self.moc)
-                ship2.name = "Defiant"
-                ship2.universe = "Star Trek"
+                let ed = Singer(context: self.moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
                 
-                let ship3 = Ship(context: self.moc)
-                ship3.name = "Millennium Falcon"
-                ship3.universe = "Star Wars"
-                
-                let ship4 = Ship(context: self.moc)
-                ship4.name = "Executor"
-                ship4.universe = "Star Wars"
+                let adele = Singer(context: self.moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
                 
                 try? self.moc.save()
             }
+            
+            Button("Show A") {
+                self.lastNameFilter = "A"
+            }
+            
+            Button("Show S") {
+                self.lastNameFilter = "S"
+            }
         }
+//        VStack {
+//            List(ships, id: \.self) { ship in
+//                Text(ship.name ?? "Unknown name")
+//            }
+//
+//            Button("Add Examples") {
+//                let ship1 = Ship(context: self.moc)
+//                ship1.name = "Enterprise"
+//                ship1.universe = "Star Trek"
+//
+//                let ship2 = Ship(context: self.moc)
+//                ship2.name = "Defiant"
+//                ship2.universe = "Star Trek"
+//
+//                let ship3 = Ship(context: self.moc)
+//                ship3.name = "Millennium Falcon"
+//                ship3.universe = "Star Wars"
+//
+//                let ship4 = Ship(context: self.moc)
+//                ship4.name = "Executor"
+//                ship4.universe = "Star Wars"
+//
+//                try? self.moc.save()
+//            }
+//        }
 //        VStack {
 //            List(wizards, id: \.self) { wizard in
 //                Text(wizard.name ?? "Unknown")
@@ -69,6 +97,20 @@ struct ContentView: View {
 //            }
 //            Text(student.name)
 //        }
+    }
+}
+
+struct FilteredList: View {
+    var fetchRequest: FetchRequest<Singer>
+    
+    var body: some View {
+        List(fetchRequest.wrappedValue, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+    
+    init(filter: String) {
+        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
     }
 }
 
---
+++ Singer+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Singer+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Dmitry Reshetnik on 25.02.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Singer {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Singer> {
+        return NSFetchRequest<Singer>(entityName: "Singer")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+    
+    var wrappedFirstName: String {
+        firstName ?? "Unknown"
+    }
+    
+    var wrappedLastName: String {
+        lastName ?? "Unknown"
+    }
+
+}
