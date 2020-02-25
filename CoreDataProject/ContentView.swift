2b28badf7f7d4f78fccc16884f3a8f6e36e496ac
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-25 22:13:57 +0200

    Filter @FetchRequest using NSPredicate

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct Student: Hashable {
     let name: String
@@ -14,29 +15,54 @@ struct Student: Hashable {
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")/*NSPredicate(format: "universe IN %@",["Aliens", "Firefly", "Star Trek"])*/) var ships: FetchedResults<Ship>
+//    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
 //    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
             
-            Button("Add") {
-                let wizard = Wizard(context: self.moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try self.moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Add Examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? self.moc.save()
             }
         }
+//        VStack {
+//            List(wizards, id: \.self) { wizard in
+//                Text(wizard.name ?? "Unknown")
+//            }
+//
+//            Button("Add") {
+//                let wizard = Wizard(context: self.moc)
+//                wizard.name = "Harry Potter"
+//            }
+//
+//            Button("Save") {
+//                do {
+//                    try self.moc.save()
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        }
 //        List(students, id: \.self) { student in
 //            if self.moc.hasChanges {
 //                //try? self.moc.save()
