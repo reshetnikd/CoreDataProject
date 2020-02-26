2acafc2dbce8c2c38e5e1d2310c1e7b1f3149967
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-26 21:58:38 +0200

    Modify FilteredList to accept NSSortDescriptor, enum as predicate parameter

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -15,81 +15,81 @@ struct Student: Hashable {
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Country.entity(), sortDescriptors: []) var countries: FetchedResults<Country>
-//    @State private var lastNameFilter = "A"
+//    @FetchRequest(entity: Country.entity(), sortDescriptors: []) var countries: FetchedResults<Country>
+    @State private var lastNameFilter = "A"
 //    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")/*NSPredicate(format: "universe IN %@",["Aliens", "Firefly", "Star Trek"])*/) var ships: FetchedResults<Ship>
 //    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
 //    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(countries, id: \.self) { country in
-                    Section(header: Text(country.wrappedFullName)) {
-                        ForEach(country.candyArray, id: \.self) { candy in
-                            Text(candy.wrappedName)
-                        }
-                    }
-                }
-            }
-            
-            Button("Add") {
-                let candy1 = Candy(context: self.moc)
-                candy1.name = "Mars"
-                candy1.origin = Country(context: self.moc)
-                candy1.origin?.shortName = "UK"
-                candy1.origin?.fullName = "United Kingdom"
-                
-                let candy2 = Candy(context: self.moc)
-                candy2.name = "KitKat"
-                candy2.origin = Country(context: self.moc)
-                candy2.origin?.shortName = "UK"
-                candy2.origin?.fullName = "United Kingdom"
-                
-                let candy3 = Candy(context: self.moc)
-                candy3.name = "Twix"
-                candy3.origin = Country(context: self.moc)
-                candy3.origin?.shortName = "UK"
-                candy3.origin?.fullName = "United Kindom"
-                
-                let candy4 = Candy(context: self.moc)
-                candy4.name = "Toblerone"
-                candy4.origin = Country(context: self.moc)
-                candy4.origin?.shortName = "CH"
-                candy4.origin?.fullName = "Switzerland"
-                
-                try? self.moc.save()
-            }
-        }
 //        VStack {
-//            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
-//                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+//            List {
+//                ForEach(countries, id: \.self) { country in
+//                    Section(header: Text(country.wrappedFullName)) {
+//                        ForEach(country.candyArray, id: \.self) { candy in
+//                            Text(candy.wrappedName)
+//                        }
+//                    }
+//                }
 //            }
 //
-//            Button("Add Examples") {
-//                let taylor = Singer(context: self.moc)
-//                taylor.firstName = "Taylor"
-//                taylor.lastName = "Swift"
+//            Button("Add") {
+//                let candy1 = Candy(context: self.moc)
+//                candy1.name = "Mars"
+//                candy1.origin = Country(context: self.moc)
+//                candy1.origin?.shortName = "UK"
+//                candy1.origin?.fullName = "United Kingdom"
 //
-//                let ed = Singer(context: self.moc)
-//                ed.firstName = "Ed"
-//                ed.lastName = "Sheeran"
+//                let candy2 = Candy(context: self.moc)
+//                candy2.name = "KitKat"
+//                candy2.origin = Country(context: self.moc)
+//                candy2.origin?.shortName = "UK"
+//                candy2.origin?.fullName = "United Kingdom"
 //
-//                let adele = Singer(context: self.moc)
-//                adele.firstName = "Adele"
-//                adele.lastName = "Adkins"
+//                let candy3 = Candy(context: self.moc)
+//                candy3.name = "Twix"
+//                candy3.origin = Country(context: self.moc)
+//                candy3.origin?.shortName = "UK"
+//                candy3.origin?.fullName = "United Kindom"
+//
+//                let candy4 = Candy(context: self.moc)
+//                candy4.name = "Toblerone"
+//                candy4.origin = Country(context: self.moc)
+//                candy4.origin?.shortName = "CH"
+//                candy4.origin?.fullName = "Switzerland"
 //
 //                try? self.moc.save()
 //            }
-//
-//            Button("Show A") {
-//                self.lastNameFilter = "A"
-//            }
-//
-//            Button("Show S") {
-//                self.lastNameFilter = "S"
-//            }
 //        }
+        VStack {
+            FilteredList(filterKey: "lastName", filterValue: lastNameFilter, sortDescriptors: [NSSortDescriptor(key: "firstName", ascending: false)], appliedPredicate: .contains) { (singer: Singer) in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            }
+
+            Button("Add Examples") {
+                let taylor = Singer(context: self.moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
+
+                let ed = Singer(context: self.moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
+
+                let adele = Singer(context: self.moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
+
+                try? self.moc.save()
+            }
+
+            Button("Show A") {
+                self.lastNameFilter = "A"
+            }
+
+            Button("Show S") {
+                self.lastNameFilter = "S"
+            }
+        }
 //        VStack {
 //            List(ships, id: \.self) { ship in
 //                Text(ship.name ?? "Unknown name")
@@ -143,6 +143,11 @@ struct ContentView: View {
 }
 
 struct FilteredList<T: NSManagedObject, Content: View>: View {
+    enum Predicate: String {
+        case beginsWith = "BEGINSWITH"
+        case contains = "CONTAINS"
+    }
+    
     var fetchRequest: FetchRequest<T>
     var singers: FetchedResults<T> { fetchRequest.wrappedValue }
     
@@ -155,8 +160,8 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    init(filterKey: String, filterValue: String, sortDescriptors: [NSSortDescriptor], appliedPredicate: Predicate, @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: sortDescriptors, predicate: NSPredicate(format: "%K \(appliedPredicate) %@", filterKey, filterValue))
         self.content = content
     }
 }
